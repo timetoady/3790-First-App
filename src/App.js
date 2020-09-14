f90dc5e9040953e30cba249234a43ccac9596945
ADAM ANDREASON <adamandreason@gmail.com>
2020-09-14 01:58:26 -0600

    updated json data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import './App.css';
-import Platforms from './data/Platforms'
-import Games from './data/Games'
+import React from "react";
+import "./App.css";
+import Platforms from "./data/Platforms";
+import Games from "./data/Games";
 
 function App() {
   return (
     <div className="App">
-    
-    <h1>Platforms and Games</h1>
-    
-  <div  className="itemDisp"> 
-    <div>
-    <Platforms />
-  </div>
+      <h1>Platforms and Games</h1>
 
-  <div>
-  <Games />
-  </div>
- 
-      
-</div>
+      <div className="itemDisp">
+        <div>
+          <Platforms />
+        </div>
+
+        <div>
+          <Games />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-//what the JSX (that looks like HTML) when it gets compiled
-//return React.createElement('div', {className: App}, React.createElement('h1', null, "This is the h1 rendered."))
