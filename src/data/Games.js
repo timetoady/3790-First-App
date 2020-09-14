92604b28987657548b5740c639f972209302a50d
ADAM ANDREASON <adamandreason@gmail.com>
2020-09-14 00:56:56 -0600

    search, search bar

--- src/data/Games.js
+++ src/data/Games.js
@@ -1,44 +1,49 @@
-import React from 'react';
-import gamesData from './games.json'
-import '../Games.css'
-//Here, we're just going to so some simple function at the 
-//top to use a functional component
+import React from "react";
+import gamesData from "./games.json";
+import "../Games.css";
 
-const allGames = gamesData.results
+const allGames = gamesData.results;
 
+function Games() {
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState([]);
+    const handleChange = event => {
+       setSearchTerm(event.target.value);
+     };
+     React.useEffect(() => {
+        const results = allGames.filter(game =>
+          game.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          game.platforms[0].platform.name.toLowerCase().includes(searchTerm.toLowerCase()) 
+        );
+        setSearchResults(results);
+      }, [searchTerm]);
 
-function Games(){
-    
-    return (
-<div>
-<h2>Very good games list ({allGames.length}): </h2>
-
-<div className="theGames">
-  {
-      allGames.map(result =>{
+  return (
+    <div>
+      <h2>Very good games list ({allGames.length}): </h2>
+      <input
+        type="search"
+        placeholder="Search games..."
+        value={searchTerm}
+        className="sbx-custom__input"
+        onChange={handleChange}
+      ></input>
+      <div className="theGames">
+        {searchResults.map((result) => {
           return (
             <div key={result.id}>
-              <h3>
-                  {result.name}
-              </h3>
+              <h3>{result.name}</h3>
               <p>Rating: {result.rating}</p>
               <p>Number of ratings: {result.ratings_count}</p>
-          <p>Platform: {result.platforms[0].platform.name}</p>
-          <p>Release date: {result.released}</p>
+              <p>Platform: {result.platforms[0].platform.name}</p>
+              <p>Release date: {result.released}</p>
               <img src={result.background_image} alt={result.name}></img>
-    
-              </div>
-          )
-      })
-  }
-  </div>
-</div>
-
-
-
-    )
-    
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-
-export default Games
+export default Games;
--- src/data/Platforms.js
+++ src/data/Platforms.js
@@ -1,77 +1,70 @@
-import React, { Component } from 'react';
-import platformData from './platforms.json'
+import React, { Component } from "react";
+import platformData from "./platforms.json";
 
 //Style for platform div
 const platStyle = {
-    display: 'grid',
-    gridTemplateColumns: '1fr',
-    gap: '1rem',
-    gridTemplateRows:  'repeat(3, minmax(0, auto))',
-    
-} 
+  display: "grid",
+  gridTemplateColumns: "1fr",
+  gap: "1rem",
+  gridTemplateRows: "repeat(3, minmax(0, auto))",
+};
 
 //Style for each platform div
 const boxStyle = {
-    border: '4px groove #AAA',
-    boxShadow: '2px 3px 2px #DDD',
-    padding: '.3rem',
-}
-
+  border: "4px groove #AAA",
+  boxShadow: "2px 3px 2px #DDD",
+  padding: ".3rem",
+};
 
 //Platforms proper
 class Platforms extends Component {
+  state = {
+    allPlatforms: platformData.results,
+  };
 
-    state = {
-        allPlatforms: platformData.results,
-
-    }
-
-    gameNumSortHandler = () => {
-        const mostPlatforms = [...this.state.allPlatforms]
-        const sortedPlats = mostPlatforms.sort((a, b) => b.games_count - a.games_count)
-        let theButton = document.querySelector(".theButton")
-    if (this.state.allPlatforms[1].name !== 'iOS') {
-        this.setState({
-            allPlatforms: sortedPlats
-          })
-          theButton.textContent = "Sort by lowest number of games:"
+  gameNumSortHandler = () => {
+    const mostPlatforms = [...this.state.allPlatforms];
+    const sortedPlats = mostPlatforms.sort(
+      (a, b) => b.games_count - a.games_count
+    );
+    let theButton = document.querySelector(".theButton");
+    if (this.state.allPlatforms[1].name !== "iOS") {
+      this.setState({
+        allPlatforms: sortedPlats,
+      });
+      theButton.textContent = "Sort by lowest number of games:";
     } else {
-        const reSortedPlats = mostPlatforms.sort((b, a) => b.games_count - a.games_count)
-        this.setState({
-            allPlatforms: reSortedPlats
-          })
-          theButton.textContent = "Sort by highest number of games:"
-        }
+      const reSortedPlats = mostPlatforms.sort(
+        (b, a) => b.games_count - a.games_count
+      );
+      this.setState({
+        allPlatforms: reSortedPlats,
+      });
+      theButton.textContent = "Sort by highest number of games:";
     }
+  };
 
-
-
-    render() {
-        return (
-
-        <div>
+  render() {
+    return (
+      <div>
         <h2>All listed platforms ({platformData.count}): </h2>
-        <button className="theButton" onClick={this.gameNumSortHandler}>Sort by highest number of games:</button>
-        {
-      this.state.allPlatforms.map(result =>{
+        <button className="theButton" onClick={this.gameNumSortHandler}>
+          Sort by highest number of games:
+        </button>
+        {this.state.allPlatforms.map((result) => {
           return (
-             <div style={platStyle} key={result.id}> 
-            <div style={boxStyle} >
-              <h3>
-                  {result.name}
-              </h3>
-              <p>Number of games: {result.games_count}</p>
-              <img src={result.image_background} alt={result.slug}></img>
+            <div style={platStyle} key={result.id}>
+              <div style={boxStyle}>
+                <h3>{result.name}</h3>
+                <p>Number of games: {result.games_count}</p>
+                <img src={result.image_background} alt={result.slug}></img>
               </div>
-              </div> 
-          )
-      })
+            </div>
+          );
+        })}
+      </div>
+    );
   }
-
-        </div>
-
-
-)}
 }
 
-export default Platforms
+export default Platforms;
