092cbac09836617fea7696a31f16a25f76fe5760
ADAM ANDREASON <adamandreason@gmail.com>
2020-12-03 01:58:55 -0700

    google and facebook db connect, fixed avatar

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -14,6 +14,7 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 import { AuthContext } from "../contexts/AuthContext";
 import { Redirect } from "react-router-dom";
+import firebase from "../lib/firebase";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,35 +28,102 @@ const useStyles = makeStyles((theme) => ({
   },
   providerIcon: {
     width: "50px",
-    marginRight: '.4rem'
-  }
+    marginRight: ".4rem",
+  },
 }));
 
 export default function FormDialog(props) {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   const authContext = useContext(AuthContext);
-  const { signInWithGoogle, signInWithEmailAndPassword, signInWithFacebook } = authContext
+  const {
+    signInWithGoogle,
+    signInWithEmailAndPassword,
+    signInWithFacebook,
+  } = authContext;
 
   const handleGoogleClick = async () => {
     try {
       await signInWithGoogle()
-      handleClose()
-    } catch(error){
-      console.error(error)
-    }
-  }
+        .then(function (user) {
+          // get user data from the auth trigger
+          const theUser = user.additionalUserInfo.profile;
+          console.log("Here's the user:", user);
+          if (user.additionalUserInfo.isNewUser === true) {
+            console.log("New user detected!");
+            const userUid = theUser.id; // The UID of the user.
+            const email = theUser.email; // The email of the user.
+            const displayName = theUser.name; // The display name of the user.
+            const userImg = theUser.picture;
+            console.log(`User email from Google Login: ${email}`);
+            // set account  doc
 
-const handleFacebookClick = async () => {
-  try{
-    await signInWithFacebook()
-    handleClose()
-  }catch(error){
-    console.log(`${error} -- came from Dialog`)
-    console.error(error)
-    alert(`${error}. Please confirm and try again.`)
-  }
-}
+            const account = {
+              useruid: userUid,
+              email,
+              userName: displayName,
+              collection: [],
+              wishlist: [],
+              avatar: userImg,
+            };
+
+            firebase.firestore().collection("users").doc(email).set(account)
+            .then(() => console.log("Successfully added user info to database."))
+          } else{
+            console.log(`Account ${user.additionalUserInfo.name}`)
+          }
+        })
+      .catch(function (error) {
+          // Handle Errors here.
+          console.log("There was an error.");
+          console.error(error);
+        });
+
+      handleClose();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleFacebookClick = async () => {
+    try {
+      await signInWithFacebook()
+        .then(function (result) {
+          console.log("Here's the facebook result:", result);
+          if (result.additionalUserInfo.isNewUser) {
+            //const token = result.credential.accessToken;
+            const userId = result.additionalUserInfo.profile.id;
+            const userName = result.additionalUserInfo.profile.name;
+            const email = result.additionalUserInfo.profile.email;
+            const imgURL = result.additionalUserInfo.profile.picture.data.url;
+            //const imgURL =`https://graph.facebook.com/me/picture?access_token=${token}`
+            const account = {
+              useruid: userId,
+              email,
+              userName: userName,
+              collection: [],
+              wishlist: [],
+              avatar: imgURL,
+            };
+            firebase.firestore().collection("users").doc(email).set(account)
+            .then(() => console.log("Successfully added user info to database."));
+          }else {
+            console.log(`Account ${result.additionalUserInfo.profile.email}`)
+          }
+        })
+        
+        .catch(function (error) {
+          // Handle Errors here.
+          console.log("There was an error.");
+          console.error(error);
+        })
+      handleClose();
+    } catch (error) {
+      console.log(`${error} -- came from Dialog`);
+      console.error(error);
+      alert(`${error}. Please confirm and try again.`);
+    }
+  };
 
   const handleLoginState = () => {
     console.log(`Auth state is ${authContext.isAuthenticated}`);
@@ -71,7 +139,6 @@ const handleFacebookClick = async () => {
       }
     }
   };
-
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -102,127 +169,134 @@ const handleFacebookClick = async () => {
           className={classes.button}
           color="primary"
           onClick={handleLoginState}
-          
         >
           Login
         </Typography>
       )}
       <div>
-      <Dialog
-        className={classes.root}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Login</DialogTitle>
-        <Button 
-        className={classes.googleButton}
-        fullWidth
-        onClick={handleGoogleClick}
-        size="large"
+        <Dialog
+          className={classes.root}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
         >
-          <img alt="Google" className= {classes.providerIcon} src='/static/images/google-icon.svg'/>
-         Login with Google
-        </Button>
-        <Button
-        className={classes.googleButton}
-        fullWidth
-        onClick={handleFacebookClick}
-        size="large"
-        >
-      <img alt="Facebook" className= {classes.providerIcon} src='/static/images/facebook-icon.svg'/>
-      Login with Facebook
-        </Button>
-        <Formik
-          initialValues={{ email: "you@email.com", password: "" }}
-          validationSchema={Yup.object().shape({
-            email: Yup.string()
-              .email("Invalid email provided.")
-              .max(30)
-              .required("Must have email to login."),
-            password: Yup.string()
-              .min(8)
-              .max(30)
-              .required("Please add your password to login."),
-          })}
-          onSubmit={ async (values, { setErrors, setStatus, setSubmitting }) => {
-            try {
-              console.log(values.email, values.password);
-              await signInWithEmailAndPassword(values.email, values.password)
-              handleClose();
-            } catch (error) {
-              console.error(error);
-              setStatus({ success: false})
-              setErrors({submit: error.message})
-              setSubmitting(false)
-            }
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <form noValidate onSubmit={handleSubmit} autoComplete="off">
-              <DialogContent>
-                <DialogContentText>
-                  Please enter your login information below.
-                </DialogContentText>
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="email"
-                  name="email"
-                  label="Email Address"
-                  type="email"
-                  fullWidth
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  error={Boolean(touched.email && errors.email)}
-                  helperText={touched.email && errors.email}
-                  required
-                  tabIndex={0}
-                />
-                <TextField
-                  margin="dense"
-                  id="password"
-                  name="password"
-                  label="Password"
-                  type="password"
-                  fullWidth
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                  error={Boolean(touched.password && errors.password)}
-                  helperText={touched.password && errors.password}
-                  required
-                  
-                />
-
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose} color="primary">
-                  Cancel
-                </Button>
-                <Button
-                  type="submit"
-                  disabled={Boolean(errors.email || errors.password)}
-                  color="primary"
-                  
-                >
-                  Confirm
-                </Button>
-              </DialogActions>
-            </form>
-          )}
-        </Formik>
-      </Dialog>
+          <DialogTitle id="form-dialog-title">Login</DialogTitle>
+          <Button
+            className={classes.googleButton}
+            fullWidth
+            onClick={handleGoogleClick}
+            size="large"
+          >
+            <img
+              alt="Google"
+              className={classes.providerIcon}
+              src="/static/images/google-icon.svg"
+            />
+            Login with Google
+          </Button>
+          <Button
+            className={classes.googleButton}
+            fullWidth
+            onClick={handleFacebookClick}
+            size="large"
+          >
+            <img
+              alt="Facebook"
+              className={classes.providerIcon}
+              src="/static/images/facebook-icon.svg"
+            />
+            Login with Facebook
+          </Button>
+          <Formik
+            initialValues={{ email: "you@email.com", password: "" }}
+            validationSchema={Yup.object().shape({
+              email: Yup.string()
+                .email("Invalid email provided.")
+                .max(30)
+                .required("Must have email to login."),
+              password: Yup.string()
+                .min(8)
+                .max(30)
+                .required("Please add your password to login."),
+            })}
+            onSubmit={async (
+              values,
+              { setErrors, setStatus, setSubmitting }
+            ) => {
+              try {
+                console.log(values.email, values.password);
+                await signInWithEmailAndPassword(values.email, values.password);
+                handleClose();
+              } catch (error) {
+                console.error(error);
+                setStatus({ success: false });
+                setErrors({ submit: error.message });
+                setSubmitting(false);
+              }
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form noValidate onSubmit={handleSubmit} autoComplete="off">
+                <DialogContent>
+                  <DialogContentText>
+                    Please enter your login information below.
+                  </DialogContentText>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="email"
+                    name="email"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    error={Boolean(touched.email && errors.email)}
+                    helperText={touched.email && errors.email}
+                    required
+                    tabIndex={0}
+                  />
+                  <TextField
+                    margin="dense"
+                    id="password"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    error={Boolean(touched.password && errors.password)}
+                    helperText={touched.password && errors.password}
+                    required
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                  <Button
+                    type="submit"
+                    disabled={Boolean(errors.email || errors.password)}
+                    color="primary"
+                  >
+                    Confirm
+                  </Button>
+                </DialogActions>
+              </form>
+            )}
+          </Formik>
+        </Dialog>
       </div>
     </div>
   );
--- src/components/ui.js
+++ src/components/ui.js
@@ -1,10 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Button, Menu, MenuItem, Avatar, Typography } from "@material-ui/core/";
 import { makeStyles } from "@material-ui/core/styles";
 import Dialog from "./Dialog";
 import Signup from "./Signup";
 import { AuthContext } from "../contexts/AuthContext";
 import { Link } from "react-router-dom";
+import firebase from "../lib/firebase";
+
 
 //Here, import LoginContext
 
@@ -36,7 +38,37 @@ export default function SimpleMenu() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
   const [dialogOpen, setDialogOpen] = useState(false);
-  //const [loginGreeting, setLoginGreeting] = useState("");
+  const [userInfo, setUserInfo] = useState({});
+  const authContext = useContext(AuthContext);
+  
+  useEffect(() => {
+    if (authContext.isAuthenticated) {
+      const getUserInfo = () => {
+        let userEmail = authContext.user.email
+        let dbUser = firebase.firestore().collection("users").doc(userEmail);
+        dbUser
+          .get()
+          .then(function (doc) {
+            if (doc.exists) {
+              console.log("Document data:", doc.data());
+              setUserInfo(doc.data());
+              
+            } else {
+              // doc.data() will be undefined in this case
+              console.log("No such document!");
+              
+            }
+          })
+          .catch(function (error) {
+            console.log("Error getting document:", error);
+          });
+      };
+      setTimeout(function() {
+        getUserInfo()
+      }, 400)
+    }
+  }, [authContext]);
+
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -50,60 +82,68 @@ export default function SimpleMenu() {
     setDialogOpen(!dialogOpen);
     handleMenuClose();
   };
-const authContext = useContext(AuthContext)
+  
   return (
-        <div className={classes.root}>
-          <Button
-            className={classes.button}
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-            
-          >
-            Menu
-          </Button>
+    <div className={classes.root}>
+      <Button
+        className={classes.button}
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Menu
+      </Button>
+      <Menu
+        className={classes.menu}
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        {/* Here, have to have menu item handle open of modal */}
+        <MenuItem
+          tabIndex={-1}
+          className={classes.menuItem}
+          onClick={handleDialogToggle}
+        >
+          <Dialog open={dialogOpen} onClose={handleDialogToggle}></Dialog>
+        </MenuItem>
+        <MenuItem
+          tabIndex={0}
+          className={classes.menuItem}
+          onClick={handleMenuClose}
+        >
+          <Signup></Signup>
+        </MenuItem>
+        <MenuItem
+          tabIndex={0}
+          className={classes.menuItem}
+          onClick={handleMenuClose}
+        >
+          About
+        </MenuItem>
+      </Menu>
+      {console.log(`Auth state: ${authContext.isAuthenticated}`)},
+      <div className={classes.root}>
+        {authContext.isAuthenticated ? (
+          <Typography>
+            <Link to="/user">{authContext.user.name}</Link>
+          </Typography>
+        ) : (
+          <Typography>Welcome, visitor!</Typography>
+        )}
 
-          <Menu
-            className={classes.menu}
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-            
-          >
-            {/* Here, have to have menu item handle open of modal */}
-            <MenuItem tabIndex={-1} className={classes.menuItem} onClick={handleDialogToggle}>
-              <Dialog  open={dialogOpen} onClose={handleDialogToggle}></Dialog>
-            </MenuItem>
-            <MenuItem tabIndex={0} className={classes.menuItem} onClick={handleMenuClose}>
-              <Signup></Signup>
-            </MenuItem>
-            <MenuItem tabIndex={0} className={classes.menuItem} onClick={handleMenuClose}>
-              About
-            </MenuItem>
-          </Menu>
-          {console.log(`Auth state: ${authContext.isAuthenticated}`)},
-          <div className={classes.root}>
-            {authContext.isAuthenticated ? (
-            <Typography><Link to="/user">{authContext.user.name}</Link></Typography>
-            ) : (
-             
-              <Typography>Welcome, visitor!</Typography>
-            )}
-
-            {authContext.isAuthenticated ? (
-            <Avatar 
-            src={authContext.user.avatar ? (authContext.user.avatar) : null} alt={authContext.user.name}
-            ></Avatar>
-            //this is where I'll hook up the avatar photo when I get it from the database context
-            ) : (
-            <Avatar></Avatar> 
-            )
-            }
-            
-          </div>
-        </div>
-    
+        {authContext.isAuthenticated ? (
+          <Avatar
+            src={userInfo.avatar}
+            alt={authContext.user.name}
+          ></Avatar>
+        ) : (
+          //this is where I'll hook up the avatar photo when I get it from the database context
+          <Avatar></Avatar>
+        )}
+      </div>
+    </div>
   );
 }
